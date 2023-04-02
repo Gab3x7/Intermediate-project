5770facf6f39e9e73f0bc6b5b7421fa4c05fe17f
Gab3x7 <karamyan.gab@yandex.ru>
2023-04-02 16:30:39 +0400

    Новый проект

---
+++ Fproject001/Program.cs
@@ -0,0 +1,20 @@
+﻿string[] M;
+Console.WriteLine("Введите массив строк через пробел");
+string line = Console.ReadLine();
+M = line.Split(' ');
+var result = new string[M.Length];
+var realSize = 0;
+foreach (var value in M)
+{
+    if (value.Length <= 3)
+    {
+        result[realSize] = value;
+        realSize++;
+    }
+}
+Console.WriteLine(string.Join(Environment.NewLine, result, 0, realSize));
+Console.ReadKey(true);
+// Примеры:
+// [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
+// [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
+// [“Russia”, “Denmark”, “Kazan”] → []
